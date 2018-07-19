6f173ab8f5a6136d079abb9b2eb859b183d338b0
Максим <tantsurm@gmail.com>
2018-07-20 01:28:47 +0300

    port-bax fixed -> PORT

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ const server = http.createServer(function (request, response) {
       console.log( request.method, request.url );
 })
 
-server.listen(process.env.port || 3000);
+server.listen(process.env.PORT || 3000);
 console.log("Server started");
