5c08f0957864ed52cbb51c306f3b9cb48039d21d
Максим <tantsurm@gmail.com>
2018-07-19 20:25:23 +0300

    Initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const http =  require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (request, response) {
+
+      if (request.url == "/style.css") {
+          response.end(fs.readFileSync('style.css', 'utf8'));
+      }
+      else {
+          response.end(fs.readFileSync('index.html', 'utf8'));
+      }
+
+      console.log( request.method, request.url );
+
+})
+
+server.listen(3000);
+console.log("Server started");
